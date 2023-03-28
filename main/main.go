ff367d5fb06935807a3fc08dc363752d53a5e9ce
ht <22350747@qq.com>
2023-03-28 13:36:12 +0800

    调整 option

--- main/main.go
+++ main/main.go
@@ -7,32 +7,38 @@ import (
 )
 
 func main() {
-	bar := progress.New(100, progress.WithFiller("❤️"), progress.WithInterval(time.Second/5))
+	opts := []progress.Option{
+		progress.WithInterval(time.Second / 5),
+		progress.WithETAFormat("2006-01-02 15:04:05"),
+		progress.WithFillerLength(50),
+	}
+
+	bar := progress.New(100, append(opts, progress.WithFiller("❤️"))...)
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Second / 50)
 		bar.Done(1)
 	}
 	bar.Finish()
-	bar = progress.New(100, progress.WithFiller("⭐️"), progress.WithInterval(time.Second/5))
+	bar = progress.New(100, append(opts, progress.WithFiller("⭐️"))...)
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Second / 50)
 		bar.Done(1)
 	}
 	bar.Finish()
-	bar = progress.New(100, progress.WithFiller("龘"), progress.WithInterval(time.Second/5))
+	bar = progress.New(100, append(opts, progress.WithFiller("龘"))...)
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Second / 50)
 		bar.Done(1)
 	}
 	bar.Finish()
-	bar = progress.New(100, progress.WithFiller("🍺"), progress.WithInterval(time.Second/5))
+	bar = progress.New(100, append(opts, progress.WithFiller("🍺"))...)
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Second / 50)
 		bar.Done(1)
 	}
 	bar.Finish()
 
-	bar = progress.New(100, progress.WithFiller(">"), progress.WithInterval(time.Second/5))
+	bar = progress.New(100, append(opts, progress.WithFiller(">"))...)
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Second / 50)
 		bar.Done(1)
